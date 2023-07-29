19ed195aa1f42806fef2a66c3c61b0c689346a21
aayio <41933025+aayio@users.noreply.github.com>
2023-07-29 22:06:32 +0800

    update pcost.py to use csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,21 +2,22 @@
 #
 # Exercise 1.27
 
+import csv
 
 def portfolio_cost(filename):
     total_cost = 0.0
 
     with open(filename, 'rt') as portfolio:
-        headers = next(portfolio)
+        rows = csv.reader(portfolio)
+        headers = next(rows)
 
-        for line in portfolio:
+        for row in rows:
             try:
-                row = line.split(',')
                 shares = int(row[1])
                 price = float(row[2])
                 total_cost += shares * price
             except ValueError:
-                print("Couldn't parse", line)
+                print("Couldn't parse", row)
         
         return total_cost
 
