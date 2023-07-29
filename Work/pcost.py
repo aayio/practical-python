65eba17a0ac74bd538eced58acdc3e480dbe6a36
aayio <41933025+aayio@users.noreply.github.com>
2023-07-29 20:27:52 +0800

    convert pcost.py into function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,15 +2,20 @@
 #
 # Exercise 1.27
 
-total_cost = 0.0
 
-with open('Data/portfolio.csv', 'rt') as portfolio:
-    headers = next(portfolio)
+def portfolio_cost(filename):
+    total_cost = 0.0
 
-    for line in portfolio:
-        row = line.split(',')
-        shares = int(row[1])
-        price = float(row[2])
-        total_cost += shares * price
-    
-    print(f'Total cost {total_cost:.2f}')
+    with open(filename, 'rt') as portfolio:
+        headers = next(portfolio)
+
+        for line in portfolio:
+            row = line.split(',')
+            shares = int(row[1])
+            price = float(row[2])
+            total_cost += shares * price
+        
+        return total_cost
+
+cost = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', cost)
