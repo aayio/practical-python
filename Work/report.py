0e975e757df33c4bfe6dcbf4f11c59e27638f02e
aayio <41933025+aayio@users.noreply.github.com>
2023-08-02 19:17:45 +0800

    modify read_portfolio to handle any order of headers

--- Work/report.py
+++ Work/report.py
@@ -13,7 +13,8 @@ def read_portfolio(filename):
         headers = next(rows)
 
         for row in rows:
-            holding = { 'name': row[0], 'shares': int(row[1]), 'price': float(row[2]) }
+            record = dict(zip(headers, row))
+            holding = { 'name': record['name'], 'shares': int(record['shares']), 'price': float(record['price']) }
             portfolio.append(holding)
 
     return portfolio
@@ -47,7 +48,11 @@ def make_report(portfolio, prices):
 
 # Tie all of this work together by adding a few additional statements to your report.py program that computes gain/loss. These statements should take the list of stocks in Exercise 2.5 and the dictionary of prices in Exercise 2.6 and compute the current value of the portfolio along with the gain/loss.
 
-portfolio = read_portfolio('Data/portfolio.csv')
+# portfolio = read_portfolio('Data/portfolio.csv')
+
+# Also works with a different set and order of headers
+portfolio = read_portfolio('Data/portfoliodate.csv')
+
 prices = read_prices('Data/prices.csv')
 
 total_cost = 0.0
@@ -88,4 +93,3 @@ report = make_report(portfolio, prices)
 for name, shares, price, change in report:
     price_str = f'${price:.2f}'
     print(f'{name:>10s} {shares:>10d} {price_str:>10} {change:>10.2f}')
-
