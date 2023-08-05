76e4a3b87a0a7487a49c5e3d0be9ae2bc964d3f0
aayio <41933025+aayio@users.noreply.github.com>
2023-08-05 11:48:17 +0800

    organise into function print_report

--- Work/report.py
+++ Work/report.py
@@ -50,6 +50,31 @@ def make_report(portfolio, prices):
     
     return report
 
+def print_report(report):
+    # Exercise 2.10: Printing a formatted table
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    header_string = '%10s %10s %10s %10s' % headers
+
+    # My way of doing this is super hacky!
+    column_width = 10
+    num_columns = len(headers)
+    separator_string = ' '.join([column_width * '-'] * num_columns)
+
+    # This is how dabeaz prints the separator string:
+    # print(('-' * 10 + ' ') * len(headers))
+
+    print(header_string)
+    print(separator_string)
+
+    # for r in report:
+    #     print('%10s %10d %10.2f %10.2f' % r)
+
+    # Another way of getting values from each tuple and another way of printing the formatted table using f-strings
+        
+    for name, shares, price, change in report:
+        price_str = f'${price:.2f}'
+        print(f'{name:>10s} {shares:>10d} {price_str:>10} {change:>10.2f}')
+
 # Tie all of this work together by adding a few additional statements to your report.py program that computes gain/loss. These statements should take the list of stocks in Exercise 2.5 and the dictionary of prices in Exercise 2.6 and compute the current value of the portfolio along with the gain/loss.
 
 # portfolio = read_portfolio('Data/portfolio.csv')
@@ -69,34 +94,6 @@ for s in portfolio:
 print(f'Current value of portfolio: {current_value:0.2f}')
 print(f'Change: {current_value - total_cost:0.2f}')
 
-# Exercise 2.10: Printing a formatted table
-
-headers = ('Name', 'Shares', 'Price', 'Change')
-
-header_string = '%10s %10s %10s %10s' % headers
-
-# My way of doing this is super hacky!
-
-column_width = 10
-
-num_columns = len(headers)
-
-separator_string = ' '.join([column_width * '-'] * num_columns)
-
-# This is how dabeaz prints the separator string:
-# print(('-' * 10 + ' ') * len(headers))
-
-print(header_string)
-
-print(separator_string)
-
 report = make_report(portfolio, prices)
 
-# for r in report:
-#     print('%10s %10d %10.2f %10.2f' % r)
-
-# Another way of getting values from each tuple and another way of printing the formatted table using f-strings
-    
-for name, shares, price, change in report:
-    price_str = f'${price:.2f}'
-    print(f'{name:>10s} {shares:>10d} {price_str:>10} {change:>10.2f}')
+print_report(report)
