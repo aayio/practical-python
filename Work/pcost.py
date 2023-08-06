a70992deb611f86bf3739c87e576675cc4a5aea5
aayio <41933025+aayio@users.noreply.github.com>
2023-08-06 19:29:17 +0800

    use read_portfolio in portfolio_cost

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,25 +2,32 @@
 #
 # Exercise 1.27
 
-import csv, sys
+import csv, sys, report
 
 def portfolio_cost(filename):
     total_cost = 0.0
 
-    with open(filename, 'rt') as portfolio:
-        rows = csv.reader(portfolio)
-        headers = next(rows)
+    # Initial implementation
+    # with open(filename, 'rt') as portfolio:
+    #     rows = csv.reader(portfolio)
+    #     headers = next(rows)
         
-        for rowno, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            try:
-                shares = int(record['shares'])
-                price = float(record['price'])
-                total_cost += shares * price
-            except ValueError:
-                print(f'Row {rowno}: Bad row: {row}')
-        
-        return total_cost
+    #     for rowno, row in enumerate(rows, start=1):
+    #         record = dict(zip(headers, row))
+    #         try:
+    #             shares = int(record['shares'])
+    #             price = float(record['price'])
+    #             total_cost += shares * price
+    #         except ValueError:
+    #             print(f'Row {rowno}: Bad row: {row}')
+    
+    # with read_portfolio
+    portfolio = report.read_portfolio(filename)
+    
+    for s in portfolio:
+        total_cost += s['shares'] * s['price']
+    
+    return total_cost
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
